6da75fe57e0e9c90679af561cdf234b40f9d588a
Grelgn <grelgn@hotmail.com>
2024-04-14 04:13:48 +0300

    Define models

---
+++ models/comment.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+	username: { type: String, required: true, maxLength: 30 },
+	text: { type: String, required: true, maxLength: 300 },
+	timestamp: { type: Date, required: true },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
---
+++ models/post.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+	title: { type: String, required: true, maxLength: 60 },
+	text: { type: String, required: true, maxLength: 25000 },
+	timestamp: { type: Date, required: true },
+	isPublished: { type: Boolean, default: false },
+});
+
+module.exports = mongoose.model("Post", PostSchema);
