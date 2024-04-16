8c4a2cc6b8bf3d568eb02b8562f6b534e4f38ed8
Grelgn <grelgn@hotmail.com>
2024-04-16 19:44:34 +0300

    Implement post API

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,19 +1,52 @@
-exports.getAllPosts = (req, res) => {
-    return res.send("GET - See all posts")
-}
+const Post = require("../models/post");
 
-exports.publishPost = (req, res) => {
-    return res.send("POST - Publish new post")
-}
+exports.getAllPosts = async (req, res) => {
+	const posts = await Post.find();
+	return res.send(posts);
+};
 
-exports.getPost = (req, res) => {
-    return res.send(`GET - See this post: posts/${req.params.postId}`)
-} 
+exports.publishPost = async (req, res) => {
+	const post = new Post({
+		title: req.body.title,
+		text: req.body.text,
+		timestamp: req.body.timestamp,
+		isPublished: req.body.isPublished,
+	});
+	await post.save();
 
-exports.updatePost = (req, res) => {
-    return res.send(`PATCH - Update this post: posts/${req.params.postId}`)
-}
+	return res.send(`The post has been saved with the ID of ${post._id}`);
+};
 
-exports.deletePost = (req, res) => {
-    return res.send(`DELETE - Delete this post: posts/${req.params.postId}`)
-}
+exports.getPost = async (req, res) => {
+	const post = await Post.findById(req.params.postId);
+	return res.send(post);
+};
+
+exports.updatePost = async (req, res) => {
+	if (req.body.title) {
+		await Post.findByIdAndUpdate(req.params.postId, { title: req.body.title });
+	}
+
+	if (req.body.text) {
+		await Post.findByIdAndUpdate(req.params.postId, { text: req.body.text });
+	}
+
+	if (req.body.timestamp) {
+		await Post.findByIdAndUpdate(req.params.postId, {
+			timestamp: req.body.timestamp,
+		});
+	}
+
+	if (req.body.isPublished) {
+		await Post.findByIdAndUpdate(req.params.postId, {
+			isPublished: req.body.isPublished,
+		});
+	}
+
+	return res.send(`The post with the ID of ${req.params.postId} has been updated`);
+};
+
+exports.deletePost = async (req, res) => {
+	await Post.findByIdAndDelete(req.params.postId);
+	return res.send(`The post with the ID of ${req.params.postId} has been deleted`);
+};
